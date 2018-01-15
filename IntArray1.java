5f9554c9291651062b0c9fac6ae502c6bd123c53
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2018-01-15 12:35:42 -0500

    Format files

--- IntArray1.java
+++ IntArray1.java
@@ -1,17 +1,14 @@
 //Create and initialize an arrary
 
-public class IntArray1
-{
-	public static void main(String[] args)
-	{
-		int list[] = {87,68,94,100,83,78,85,91,76,87}; //declare an int array
+public class IntArray1 {
+	public static void main(String[] args) {
+		int list[] = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 }; //declare an int array
 		int counter; //declare and index for the array
-		
+
 		//list = new int [10]; //create room for 10 int elements
-		
-		for (counter = 0; counter < list.length; counter++)
-		{
-			System.out.println("["+counter+"] "+list[counter]);
+
+		for (counter = 0; counter < list.length; counter++) {
+			System.out.println("[" + counter + "] " + list[counter]);
 		}
 	}
 }
